603c56c47289db1bf4f35263ac250ab3b0b0909c
jjang051 <flyjjang051@gmail.com>
2022-11-02 11:40:57 +0900

    tab

---
+++ pc/js/common.js
@@ -0,0 +1,15 @@
+const tab = $(".tab");
+const tabMenu = $(".tab .menu li");
+const tabContents = $(".tab .contents > li");
+
+tabMenu.on("click", function () {
+  const idx = $(this).index();
+  console.log("🚀 ~ file: common.js ~ line 7 ~ idx", idx);
+  const siblings = $(this).siblings();
+  $(this).addClass("on");
+  siblings.removeClass("on");
+  const selectedContents = tabContents.eq(idx);
+  const contentsSiblings = selectedContents.siblings();
+  selectedContents.addClass("on");
+  contentsSiblings.removeClass("on");
+});
